4321d5eee3359b7b332eff48efa2f7564e3692b3
GertFreiberger <gert.freiberger@student.tugraz.at>
2016-04-20 22:42:01 +0200

    [DV,GF] Added basic functionality to main menu.

--- DrinkingGamesCollection/app/src/androidTest/java/wtfisandroid/drinkinggamescollection/testUI/MainMenuUItests.java
+++ DrinkingGamesCollection/app/src/androidTest/java/wtfisandroid/drinkinggamescollection/testUI/MainMenuUItests.java
@@ -1,8 +1,45 @@
 package wtfisandroid.drinkinggamescollection.testUI;
 
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import com.robotium.solo.Solo;
 import wtfisandroid.drinkinggamescollection.MainMenu;
+import wtfisandroid.drinkinggamescollection.R;
+
+
+public class MainMenuUItests extends ActivityInstrumentationTestCase2<MainMenu> {
+
+    private Solo mySolo;
+
+
+    public MainMenuUItests() {
+        super(MainMenu.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testButtons() {
+
+        Button button_test = (Button) mySolo.getCurrentActivity().findViewById(R.id.button_maexchen);
+        mySolo.clickOnButton(button_test.getText().toString());
+        mySolo.goBack();
+        button_test = (Button) mySolo.getCurrentActivity().findViewById(R.id.button_kingscup);
+        mySolo.clickOnButton(button_test.getText().toString());
+        mySolo.goBack();
+    }
 
 
 
-public class MainMenuUItests extends MainMenu {
+
+
+
+
+
 }
--- DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/MainMenu.java
+++ DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/MainMenu.java
@@ -1,5 +1,6 @@
 package wtfisandroid.drinkinggamescollection;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button button_mainmenu_maexchen;
+    private Button button_mainmenu_kingscup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,11 @@ public class MainMenu extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        button_mainmenu_maexchen = (Button) findViewById(R.id.button_maexchen);
+        button_mainmenu_kingscup = (Button) findViewById(R.id.button_kingscup);
 
+        button_mainmenu_maexchen.setOnClickListener(this);
+        button_mainmenu_kingscup.setOnClickListener(this);
     }
 
     @Override
@@ -41,5 +52,25 @@ public class MainMenu extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View v) {
+        Button clicked_button = (Button) v;
+
+        switch(clicked_button.getId()) {
+            case R.id.button_maexchen:
+                Intent intent_maexchen_menu = new Intent(this, MaexchenMenu.class);
+                startActivity(intent_maexchen_menu);
+                break;
+
+            case R.id.button_kingscup:
+                Intent intent_kingscup_menu = new Intent(this, KingscupMenu.class);
+                startActivity(intent_kingscup_menu);
+                break;
+
+            default:
+
+        }
+
     }
 }
