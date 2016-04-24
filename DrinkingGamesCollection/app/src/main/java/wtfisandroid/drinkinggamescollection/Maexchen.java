94a7c4f6f8c97086a19e15a4ae56870045512675
GertFreiberger <gert.freiberger@student.tugraz.at>
2016-04-24 16:10:05 +0200

    [DV, GF] create Dices, activate throw dices with Acceleometer

--- DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/Maexchen.java
+++ DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/Maexchen.java
@@ -1,29 +1,46 @@
 package wtfisandroid.drinkinggamescollection;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.Random;
 
-public class Maexchen extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
 
-    private ImageView dice_left_;
-    private ImageView dice_right_;
+import wtfisandroid.drinkinggamescollection.features.Dice;
+
+public class Maexchen extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
+
+    private ImageView dice_image_left_;
+    private ImageView dice_image_right_;
     private Button button_left_;
     private Button button_right_;
     private Button button_help_;
     private StatesMaexchen state_maexchen_;
     private TextView dice_result_;
-    private Random number_generator_;
+    private Dice dice_left_;
+    private Dice dice_right_;
+    private SensorManager sensor_manager_;
+    private Sensor sensor_accelerometer_;
+    private long last_update_;
+    private float last_x_;
+    private float last_y_;
+    private float last_z_;
+    private static final int SHAKE_THRESHOLD_ = 50;
+    private static final int WAITING_SHAKE_ = 100;
 
     public enum StatesMaexchen {
         FIRST_THROW, THROW_RESULT , SECOND_THROW, TRUST, UNCOVER
@@ -41,19 +58,40 @@ public class Maexchen extends AppCompatActivity implements View.OnClickListener 
         button_left_ = (Button) findViewById(R.id.maexchen_button_left);
         button_right_ = (Button) findViewById(R.id.maexchen_button_right);
         button_help_ = (Button) findViewById(R.id.maexchen_button_help);
-        dice_left_ = (ImageView) findViewById(R.id.imageView_dice_left);
-        dice_right_ = (ImageView) findViewById(R.id.imageView_dice_right);
+
+        ArrayList<Bitmap> pictures = new ArrayList<Bitmap>();
+        createDicePictures(pictures);
+
+        dice_left_ = new Dice(pictures);
+        dice_right_ = new Dice(pictures);
+        dice_image_left_ = (ImageView) findViewById(R.id.imageView_dice_left);
+        dice_image_right_ = (ImageView) findViewById(R.id.imageView_dice_right);
         dice_result_ = (TextView) findViewById(R.id.maexchen_textview_dice);
-        number_generator_ = new Random();
-        Bitmap left_dice = BitmapFactory.decodeResource(getResources(),R.mipmap.dice_1);
-        Bitmap right_dice = BitmapFactory.decodeResource(getResources(),R.mipmap.dice_2);
-        dice_left_.setImageBitmap(left_dice);
-        dice_right_.setImageBitmap(right_dice);
+
+        dice_left_.wuerfeln(dice_image_left_);
+        dice_right_.wuerfeln(dice_image_right_);
+        showThrowResult(dice_left_.getLastThrow() + 1, dice_right_.getLastThrow() + 1);
+
+        last_update_ = 0;
+        sensor_manager_ = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor_accelerometer_ = sensor_manager_.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor_manager_.registerListener(this, sensor_accelerometer_, SensorManager.SENSOR_DELAY_NORMAL);
         
         button_help_.setOnClickListener(this);
         button_left_.setOnClickListener(this);
         button_right_.setOnClickListener(this);
     }
+
+    public void createDicePictures(ArrayList<Bitmap> pictures) {
+        pictures.add(BitmapFactory.decodeResource(getResources(), R.mipmap.dice_1));
+        pictures.add(BitmapFactory.decodeResource(getResources(), R.mipmap.dice_2));
+        pictures.add(BitmapFactory.decodeResource(getResources(), R.mipmap.dice_3));
+        pictures.add(BitmapFactory.decodeResource(getResources(), R.mipmap.dice_4));
+        pictures.add(BitmapFactory.decodeResource(getResources(), R.mipmap.dice_5));
+        pictures.add(BitmapFactory.decodeResource(getResources(), R.mipmap.dice_6));
+    }
+
+    // For the Buttons events
 
     public void onClick(View v) {
         Button clicked_button = (Button) v;
@@ -77,21 +115,23 @@ public class Maexchen extends AppCompatActivity implements View.OnClickListener 
         }
     }
 
-
     public void buttonLeftClicked() {
 
         switch (state_maexchen_) {
             case FIRST_THROW:
+                onPause();
                 state_maexchen_ = StatesMaexchen.THROW_RESULT;
                 button_left_.setText(R.string.maexchen_button_throw_again);
                 break;
 
             case THROW_RESULT:
+                onResume();
                 state_maexchen_ = StatesMaexchen.SECOND_THROW;
                 button_left_.setVisibility(View.INVISIBLE);
                 break;
 
             case TRUST:
+                onResume();
                 state_maexchen_ = StatesMaexchen.FIRST_THROW;
                 button_left_.setText(R.string.maexchen_button_reveal);
                 button_right_.setText(R.string.maexchen_button_next);
@@ -106,18 +146,21 @@ public class Maexchen extends AppCompatActivity implements View.OnClickListener 
 
         switch (state_maexchen_) {
             case FIRST_THROW:
+                onPause();
                 state_maexchen_ = StatesMaexchen.TRUST;
                 button_left_.setText(R.string.maexchen_button_throw);
                 button_right_.setText(R.string.maexchen_button_reveal);
                 break;
 
             case THROW_RESULT:
+                onPause();
                 state_maexchen_ = StatesMaexchen.TRUST;
                 button_left_.setText(R.string.maexchen_button_throw);
                 button_right_.setText(R.string.maexchen_button_reveal);
                 break;
 
             case SECOND_THROW:
+                onPause();
                 state_maexchen_ = StatesMaexchen.TRUST;
                 button_left_.setVisibility(View.VISIBLE);
                 button_left_.setText(R.string.maexchen_button_throw);
@@ -125,12 +168,14 @@ public class Maexchen extends AppCompatActivity implements View.OnClickListener 
                 break;
 
             case TRUST:
+                onPause();
                 state_maexchen_ = StatesMaexchen.UNCOVER;
                 button_left_.setVisibility(View.INVISIBLE);
                 button_right_.setText(R.string.maexchen_button_next);
                 break;
 
             case UNCOVER:
+                onResume();
                 state_maexchen_ = StatesMaexchen.FIRST_THROW;
                 button_left_.setVisibility(View.VISIBLE);
                 button_left_.setText(R.string.maexchen_button_reveal);
@@ -140,6 +185,68 @@ public class Maexchen extends AppCompatActivity implements View.OnClickListener 
             default:
 
         }
+    }
+
+    // For the Sensor events
+
+    public void onSensorChanged(SensorEvent event) {
+
+        Sensor active_sensor = event.sensor;
+
+        if(active_sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x_acceleration = event.values[0];
+            float y_acceleration = event.values[1];
+            float z_acceleration = event.values[2];
+
+            long current_time = System.currentTimeMillis();
+
+            if((current_time - last_update_) > WAITING_SHAKE_) {
+                last_update_ = current_time;
+
+                float speed = Math.abs(x_acceleration + y_acceleration + z_acceleration - last_x_ -last_y_ -last_z_);
+
+                if(speed > SHAKE_THRESHOLD_) {
+                    dice_left_.wuerfeln(dice_image_left_);
+                    dice_right_.wuerfeln(dice_image_right_);
+                    showThrowResult(dice_left_.getLastThrow() + 1, dice_right_.getLastThrow() + 1);
+                }
+
+                last_x_ = x_acceleration;
+                last_y_ = y_acceleration;
+                last_z_ = z_acceleration;
+            }
+        }
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onPause() {
+        super.onPause();
+        sensor_manager_.unregisterListener(this);
+    }
+
+    public void onResume() {
+        super.onResume();
+        sensor_manager_.registerListener(this, sensor_accelerometer_, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void showThrowResult(int left_throw,int right_throw) {
+
+        if(left_throw == right_throw) {
+            dice_result_.setText(Integer.toString(left_throw) + "ser pasch");
+        }
+        else if((left_throw == 1 && right_throw == 2) || (left_throw == 2 && right_throw == 1)) {
+            dice_result_.setText("Mäxchen");
+        }
+        else if(left_throw < right_throw) {
+            dice_result_.setText(Integer.toString(right_throw) + Integer.toString(left_throw));
+        }
+        else {
+            dice_result_.setText(Integer.toString(left_throw) + Integer.toString(right_throw));
+        }
+
     }
 
 }
---
+++ DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/features/Dice.java
@@ -0,0 +1,42 @@
+package wtfisandroid.drinkinggamescollection.features;
+
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import wtfisandroid.drinkinggamescollection.R;
+
+
+public class Dice {
+
+    private ArrayList<Bitmap> dice_pictures_;
+    private int last_throw_;
+    private Random throw_dice_;
+    private int dice_sides_;
+
+    public Dice(ArrayList<Bitmap> pictures) {
+        last_throw_ = 0;
+        throw_dice_ = new Random();
+        dice_sides_ = 5;
+
+        dice_pictures_ = pictures;
+    }
+
+    public int getLastThrow() {
+        return last_throw_;
+    }
+
+    public void wuerfeln(ImageView image) {
+        last_throw_ = throw_dice_.nextInt(dice_sides_);
+        image.setImageBitmap(dice_pictures_.get(last_throw_));
+    }
+
+
+
+
+}
