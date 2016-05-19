d54a6215f375754b34a1aa87bee98fe51980e887
GertFreiberger <gert.freiberger@student.tugraz.at>
2016-05-19 13:44:47 +0200

    [DV, GF] begin Design of Game Menu

--- DrinkingGamesCollection/app/src/androidTest/java/wtfisandroid/drinkinggamescollection/testUI/PolnischesTrinkspielUItests.java
+++ DrinkingGamesCollection/app/src/androidTest/java/wtfisandroid/drinkinggamescollection/testUI/PolnischesTrinkspielUItests.java
@@ -1,11 +1,13 @@
 package wtfisandroid.drinkinggamescollection.testUI;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 
 import com.robotium.solo.Solo;
 
 import wtfisandroid.drinkinggamescollection.Maexchen;
 import wtfisandroid.drinkinggamescollection.PolnischesTrinkspiel;
+import wtfisandroid.drinkinggamescollection.R;
 
 public class PolnischesTrinkspielUItests extends ActivityInstrumentationTestCase2<PolnischesTrinkspiel> {
 
@@ -25,8 +27,23 @@ public class PolnischesTrinkspielUItests extends ActivityInstrumentationTestCase
     }
 
     public void testButtons() {
-        Button button_start = (Button) mypolnischSolo.getCurrentActivity().findViewById(R.id.);
+        Button button_start = (Button) mypolnischSolo.getCurrentActivity().findViewById(R.id.polnisch_button_start);
+        Button button_plus = (Button) mypolnischSolo.getCurrentActivity().findViewById(R.id.polnisch_button_plus);
+        Button button_minus= (Button) mypolnischSolo.getCurrentActivity().findViewById(R.id.polnisch_button_minus);
+        Button button_help = (Button) mypolnischSolo.getCurrentActivity().findViewById(R.id.polnisch_button_help);
 
+        mypolnischSolo.clickOnButton(button_start.getText().toString());
+        mypolnischSolo.clickOnButton(button_plus.getText().toString());
+        mypolnischSolo.clickOnButton(button_minus.getText().toString());
+        mypolnischSolo.clickOnButton(button_help.getText().toString());
+
+        String left_arrow = "Linker Pfeil ";
+        String right_arrow = "Rechter Pfeil ";
+
+        for(int i = 1; i  < 11;i++) {
+            mypolnischSolo.clickOnButton(left_arrow + i);
+            mypolnischSolo.clickOnButton(right_arrow + i);
+        }
     }
 
 
