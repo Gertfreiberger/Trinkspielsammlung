d63f27e383bac65c34ea9b0f97b1a24fa6d220a3
DevStruh <struger93@gmx.at>
2016-06-08 15:12:38 +0200

    [PS, MB] Pyramid Second Round Logic completed, Graphics and Design, Pyramid First Round Logic some minor changes, Adapted Images

--- DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/data/Playerhand.java
+++ DrinkingGamesCollection/app/src/main/java/wtfisandroid/drinkinggamescollection/data/Playerhand.java
@@ -6,70 +6,71 @@ import android.os.Parcelable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerHand implements Parcelable{
+public class PlayerHand implements Parcelable {
 
-	private int playerID = -1;
-	private String playerName = null;
-	private List<Gamecard> playerCards = new ArrayList<>();
+    private int playerID = -1;
+    private String playerName = null;
+    private List<Gamecard> playerCards = new ArrayList<>();
 
-	public PlayerHand(int playerID, String playerName) {
-		this.playerName = playerName;
-		this.playerID = playerID;
-	}
+    public PlayerHand(int playerID, String playerName) {
+        this.playerName = playerName;
+        this.playerID = playerID;
+    }
 
-	protected PlayerHand(Parcel in) {
-		playerID = in.readInt();
-		playerName = in.readString();
-		playerCards = in.createTypedArrayList(Gamecard.CREATOR);
-	}
+    protected PlayerHand(Parcel in) {
+        playerID = in.readInt();
+        playerName = in.readString();
+        playerCards = in.createTypedArrayList(Gamecard.CREATOR);
+    }
 
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeInt(playerID);
-		dest.writeString(playerName);
-		dest.writeTypedList(playerCards);
-	}
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(playerID);
+        dest.writeString(playerName);
+        dest.writeTypedList(playerCards);
+    }
 
-	@Override
-	public int describeContents() {
-		return 0;
-	}
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
-	public static final Creator<PlayerHand> CREATOR = new Creator<PlayerHand>() {
-		@Override
-		public PlayerHand createFromParcel(Parcel in) {
-			return new PlayerHand(in);
-		}
+    public static final Creator<PlayerHand> CREATOR = new Creator<PlayerHand>() {
+        @Override
+        public PlayerHand createFromParcel(Parcel in) {
+            return new PlayerHand(in);
+        }
 
-		@Override
-		public PlayerHand[] newArray(int size) {
-			return new PlayerHand[size];
-		}
-	};
+        @Override
+        public PlayerHand[] newArray(int size) {
+            return new PlayerHand[size];
+        }
+    };
 
-	public List<Gamecard> getPlayerCards() {
-		return playerCards;
-	}
+    public List<Gamecard> getPlayerCards() {
+        return playerCards;
+    }
 
-	public String getPlayerName() {
-		return playerName;
-	}
+    public String getPlayerName() {
+        return playerName;
+    }
 
-	public int getPlayerID() {
-		return playerID;
-	}
+    public int getPlayerID() {
+        return playerID;
+    }
 
-	public List<Gamecard> addCard(Gamecard card) {
-		playerCards.add(card);
-		return playerCards;
-	}
 
-	@Override
-	public String toString() {
-		return "PlayerHand{" +
-						"playerID=" + playerID +
-						", player_cards=" + playerCards +
-						'}';
-	}
+    @Override
+    public String toString() {
+        return "PlayerHand{" +
+                "playerID=" + playerID +
+                ", player_cards=" + playerCards +
+                '}';
+    }
+
+    public List<Gamecard> addCard(Gamecard card) {
+        playerCards.add(card);
+        return playerCards;
+    }
 
 }
